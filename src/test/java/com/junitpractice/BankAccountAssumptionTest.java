b31bde7ec6aac4752575f62042a132a3d8d6dd90
Mohan Lal <mhnuk2007@gmail.com>
2026-02-03 01:08:27 +0500

    implement assumption test

--- src/test/java/com/junitpractice/BankAccountAssumptionTest.java
+++ src/test/java/com/junitpractice/BankAccountAssumptionTest.java
@@ -1,4 +1,19 @@
 package com.junitpractice;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
 public class BankAccountAssumptionTest {
+
+    @Test
+    @DisplayName("Test activation account after creation")
+    public void  testActivationAccountAfterCreation() {
+        BankAccount bankAccount = new BankAccount(500, -1000);
+        assumeFalse(bankAccount == null);
+        assertTrue(bankAccount.isActive());
+    }
 }
