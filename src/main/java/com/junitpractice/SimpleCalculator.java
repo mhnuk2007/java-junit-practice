440a414666bda6b38b6624310c813eaa80841f14
Mohan Lal <mhnuk2007@gmail.com>
2026-02-02 13:16:40 +0500

    Add test for all arthemitic operations

--- src/main/java/com/junitpractice/SimpleCalculator.java
+++ src/main/java/com/junitpractice/SimpleCalculator.java
@@ -4,4 +4,16 @@ public class SimpleCalculator {
     public int add(int a, int b){
         return a+b;
     }
+
+    public int sub(int a, int b){
+        return a-b;
+    }
+
+    public int mul(int a, int b){
+        return a*b;
+    }
+
+    public double div(int a, int b){
+        return a/b;
+    }
 }
--- src/test/java/com/junitpractice/SimpleCalculatorTest.java
+++ src/test/java/com/junitpractice/SimpleCalculatorTest.java
@@ -3,6 +3,7 @@ package com.junitpractice;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SimpleCalculatorTest {
 
@@ -10,5 +11,22 @@ public class SimpleCalculatorTest {
     @Test
     public void testAdd() {
         assertEquals(8, simpleCalculator.add(5, 3));
+    }
+    @Test
+    public void testSub() {
+        assertEquals(2, simpleCalculator.sub(5, 3));
+    }
+    @Test
+    public void testMultiply() {
+        assertEquals(15, simpleCalculator.mul(5, 3));
+    }
+    @Test
+    public void testDivide() {
+        assertEquals(2, simpleCalculator.div(6, 3));
+    }
+
+    @Test
+    public void testDivideByZero() {
+        assertThrows(ArithmeticException.class, () -> simpleCalculator.div(5, 0));
     }
 }
