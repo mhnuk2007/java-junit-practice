6aa05dfcf17b4aa6136454361149df3bfa9e9914
Mohan Lal <mhnuk2007@gmail.com>
2026-02-02 23:19:48 +0500

    Implement displayname in unit test

--- src/test/java/com/junitpractice/BankAccountTest.java
+++ src/test/java/com/junitpractice/BankAccountTest.java
@@ -1,17 +1,20 @@
 package com.junitpractice;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BankAccountTest {
     @Test
+    @DisplayName("Withdraw 300 successfully")
     public void testWithDraw(){
         BankAccount bankAccount = new BankAccount(500, -1000);
         assertEquals(300, bankAccount.withdraw(200));
     }
 
     @Test
+    @DisplayName("Deposit 200 successfully")
     public void testDeposit(){
         BankAccount bankAccount = new BankAccount(400, 0);
         bankAccount.deposit(200);
