b015c357fcf0dcdb22891c2d61a1069f9dcddbd2
Mohan Lal <mhnuk2007@gmail.com>
2026-02-04 00:42:35 +0500

    Add assumptions and ordered execution tests

--- src/test/java/com/junitpractice/BankAccountAssumptionTest.java
+++ src/test/java/com/junitpractice/BankAccountAssumptionTest.java
@@ -3,9 +3,9 @@ package com.junitpractice;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assumptions.assumeFalse;
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.*;
 
 public class BankAccountAssumptionTest {
 
@@ -13,7 +13,12 @@ public class BankAccountAssumptionTest {
     @DisplayName("Test activation account after creation")
     public void  testActivationAccountAfterCreation() {
         BankAccount bankAccount = new BankAccount(500, -1000);
+//        assumeTrue(bankAccount != null);
+//        assertTrue(bankAccount.isActive());
+
         assumeFalse(bankAccount == null);
         assertTrue(bankAccount.isActive());
+
+        assumingThat(bankAccount != null, () -> assertTrue(bankAccount.isActive()));
     }
 }
